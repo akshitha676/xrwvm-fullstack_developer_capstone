533b9cbf03ee900683bedaa981ed76be22d2c95e
akshitha676 <akshithapulaboyina@gmail.com>
2026-03-01 01:32:38 -0500

    Added login, logout, and registration functionality with React frontend

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,25 @@
-import LoginPanel from "./components/Login/Login"
+// import LoginPanel from "./components/Login/Login"
+// import { Routes, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/login" element={<LoginPanel />} />
+//     </Routes>
+//   );
+// }
+// export default App;
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";  // Add this import
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />  {/* Added Register route */}
     </Routes>
   );
 }
+
 export default App;
